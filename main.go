5e72c6b50333678258d59092fb2010347fda3d00
Carlos Durante <carlos.durante@outlook.com>
2022-01-31 13:21:54 -0300

    feat: start server and database

--- main.go
+++ main.go
@@ -1,4 +1,14 @@
 package main
 
+import (
+	"github.com/carloshdurante/api_golang/database"
+	"github.com/carloshdurante/api_golang/server"
+)
+
 func main() {
+	database.StartDB()
+
+	server := server.NewServer()
+
+	server.Run()
 }
