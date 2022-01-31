daa4bb52595a3c81dd5f903502361523e33b4c43
Carlos Durante <carlos.durante@outlook.com>
2022-01-31 13:22:48 -0300

    feat: server config

--- server/server.go
+++ server/server.go
@@ -1,17 +1,26 @@
 package server
 
+import (
+	"log"
+
+	"github.com/carloshdurante/api_golang/server/routes"
+	"github.com/gin-gonic/gin"
+)
+
 type Server struct {
-	port string
+	port   string
 	server *gin.Engine
 }
 
 func NewServer() Server {
 	return Server{
-		port: 8090,
+		port:   "8090",
 		server: gin.Default(),
 	}
 }
 
 func (s *Server) Run() {
-	s.server.Run(":" + s.port)
+	router := routes.ConfigRoutes(s.server)
+	log.Print("Server running on port: ", s.port)
+	log.Fatal(router.Run(":" + s.port))
 }
