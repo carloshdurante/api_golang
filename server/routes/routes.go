27912f35f33d1c58bb309b91cca309e1b3ec12c3
Carlos Durante <carlos.durante@outlook.com>
2022-01-31 13:25:01 -0300

    feat: routes config

--- server/routes/routes.go
+++ server/routes/routes.go
@@ -1,5 +1,22 @@
 package routes
 
-func ConfigRoutes(router *gin.Engine) *gin.Engine {
+import (
+	"github.com/carloshdurante/api_golang/controllers"
+	"github.com/gin-gonic/gin"
+)
 
+func ConfigRoutes(router *gin.Engine) *gin.Engine {
+	main := router.Group("api/v1")
+	{
+		books := main.Group("books")
+		{
+			books.GET("/:id", controllers.ShowBook)
+			books.POST("/", controllers.CreateBook)
+			books.DELETE("/:id", controllers.DeleteBook)
+			books.PUT("/:id", controllers.UpdateBook)
+			books.GET("/", controllers.ShowAllBooks)
+		}
+	}
+
+	return router
 }
