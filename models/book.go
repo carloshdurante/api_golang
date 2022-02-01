287ba29a6336dbf7f15615cbff09119aad57476b
Carlos Durante <carlos.durante@outlook.com>
2022-02-01 14:12:04 -0300

    feat: default not null

--- models/book.go
+++ models/book.go
@@ -7,9 +7,9 @@ import (
 )
 
 type Book struct {
-	ID        uint           `gorm:"primary_key;auto_increment" json:"id"`
-	Title     string         `gorm:"not null" json:"title"`
-	Author    string         `gorm:"not null" json:"author"`
+	ID        uint           `gorm:"primary_key;auto_increment;unique" json:"id"`
+	Title     string         `gorm:"not null;default:null" json:"title"`
+	Author    string         `gorm:"not nulldefault:null" json:"author"`
 	CreatedAt time.Time      `json:"created_at"`
 	UpdatedAt time.Time      `json:"updated_at"`
 	DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted_at"`
